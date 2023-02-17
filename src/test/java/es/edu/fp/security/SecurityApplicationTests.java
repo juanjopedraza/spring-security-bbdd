912185d7726e151844533fb8a4bd4ff7d03ae639
juanjo <dam@daw.com.es>
2023-02-17 21:58:03 +0100

    Spring Security

---
+++ src/test/java/es/edu/fp/security/SecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package es.edu.fp.security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
